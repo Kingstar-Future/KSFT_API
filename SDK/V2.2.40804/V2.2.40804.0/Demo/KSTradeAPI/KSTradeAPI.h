c31b6b8bf9cd9254ebc2c6b7cb38ae4e4b81af75
Kingstar-Future <ksftapi@gmail.com>
2014-08-05 10:56:54 +0800

    做市商支持&VOC接口
    voice of  customer

---
+++ SDK/V2.2.40804/V2.2.40804.0/Demo/KSTradeAPI/KSTradeAPI.h
@@ -0,0 +1,21 @@
+
+// The following ifdef block is the standard way of creating macros which make exporting 
+// from a DLL simpler. All files within this DLL are compiled with the KSTRADEAPI_EXPORTS
+// symbol defined on the command line. this symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see 
+// KSTRADEAPI_API functions as being imported from a DLL, wheras this DLL sees symbols
+// defined with this macro as being exported.
+#ifndef __KSTRADE_API_H__
+#define __KSTRADE_API_H__
+#ifdef KSTRADEAPI_EXPORTS
+#define KSTRADEAPI_API __declspec(dllexport)
+#else
+#define KSTRADEAPI_API __declspec(dllimport)
+#endif
+
+#include "../Inc/KSTraderApiEx.h"
+#include "../Inc/KSCosApi.h"
+#include "../Inc/KSOptionApi.h"
+#include "../Inc/KSVocApi.h"
+
+#endif
